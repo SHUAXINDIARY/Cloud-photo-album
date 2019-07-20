1d0bfdc155d81ebe43d7eea67ac63fa54ad6c69f
shuaxin <986384495@qq.com>
2019-07-20 13:43:22 +0800

    更新

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,91 +12,103 @@ Page({
   // 1.单张图片上传
   upload() {
     let that = this;
-    // 获取传递参数 event.currentTarget.dataset.imgnumber
-    feature.upload().then(
-      (res) => {
-        console.log(res);
-        // 隐藏加载提示
-        wx.hideLoading();
-        until.hintResult('上传成功');
-      }
-    )
+    if (this.checkStaus() == 0) {
+      that.checkStaus()
+    } else {
+      // 获取传递参数 event.currentTarget.dataset.imgnumber
+      feature.upload().then(
+        (res) => {
+          console.log(res);
+          // 隐藏加载提示
+          wx.hideLoading();
+          until.hintResult('上传成功');
+        }
+      )
+    }
+
   },
   //1.5 多张图片上传
   uploads() {
     let that = this;
-    feature.uploads().then(
-      (res) => {
-        // 隐藏加载提示
-        wx.hideLoading();
-        until.hintResult('上传成功');
-      }
-    );
+    if (this.checkStaus() == 0) {
+      that.checkStaus()
+    } else {
+      feature.uploads().then(
+        (res) => {
+          // 隐藏加载提示
+          wx.hideLoading();
+          until.hintResult('上传成功');
+        }
+      );
+    }
+
     // console.log(that.data.imgs);
+  },
+  // 检查登录状态
+  checkStaus() {
+    // 通过全局变量userInfo检查是否登陆
+    if (app.globalData.userInfo == null) {
+      wx.showToast({
+        icon: 'none',
+        title: '请先登录',
+      });
+      return 0;
+    };
+    return 1;
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
-    // 通过全局变量userInfo检查是否登陆
-    if (app.globalData.userInfo==null) {
-      wx.switchTab({
-        url: "../login/login"
-      });
-      wx.showToast({
-        icon: 'none',
-        title: '请先登录',
-      });
-    }
-    console.log(app.globalData.userInfo);
+  onShow: function () {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
