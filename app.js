241a4a6cdcffad0d4373b75fc0a9ff87a4517963
shuaxin <986384495@qq.com>
2019-07-20 22:04:44 +0800

    更新

---
+++ app.js
@@ -0,0 +1,18 @@
+//app.js
+App({
+  // 程序启动函数
+  /**小程序函数不卸载methods里，和data'并列 */
+  onLaunch: function() {
+    /**初始化云开发环境 */
+    wx.cloud.init({
+      // 是否在将用户访问记录到用户管理中，在控制台中可见
+      traceUser:true,
+      // 设置云开发环境  后面填你的云存储环境id
+      env:"shauxin-chy42"
+    });
+  },
+  // 全局变量
+  globalData: {
+    userInfo: null
+  }
+})
